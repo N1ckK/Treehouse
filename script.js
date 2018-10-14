ff3b7e3979bb9cfd0b5d168dae01e80469149f11
Nick Seinsche <xdgamer3110@gmail.com>
2018-10-14 11:33:36 +0200

    Changes site title and icon. Improved site resize response

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+// changes quote on start
 $.getJSON("https://talaikis.com/api/quotes/random/", function(a) {
     $("#id_quote").text(a.quote);
     $("#id_quote_author").text(a.author);
 });
 
+// changes quote on a fixed interval
 window.setInterval(function(){
     $.getJSON("https://talaikis.com/api/quotes/random/", function(a) {
 
@@ -19,10 +20,27 @@ window.setInterval(function(){
     });
 }, 1000*15);
 
+// manages page resizing
 window.addEventListener('resize', function(event){
     if ($(window).width() < 800){
-        $("#title").css('display', 'None');
+        // SMALL
+        // title fades out
+        $("#title").fadeOut( "fast", function(a) {
+            $('#title').css('display', 'None');
+        });
+        // navigator buttons less spaced
+        $('.navigators').find('a').css('padding', '5px 1vw 5px 1vw');
+        // navigator right space removed
+        $('.navigators').css('right', '5px');
     }else{
-        $("#title").css('display', 'Block');
+        // NORMAL
+        // title fades in
+        $("#title").fadeIn( "fast", function(a) {
+            $('#title').css('display', 'Block');
+        });
+        // navigator buttons are wider spaced
+        $('.navigators').find('a').css('padding', '5px 2vw 5px 2vw');
+        // navigator section gets right space
+        $('.navigators').css('right', '50px');
     }
 });
