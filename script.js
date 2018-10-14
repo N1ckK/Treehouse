0e41a1f1cfb3f0fcf51b2b001f36a4259e1bfe76
Nick Seinsche <xdgamer3110@gmail.com>
2018-10-14 18:53:12 +0200

    Added cycle throughs

--- script.js
+++ script.js
@@ -20,8 +20,7 @@ window.setInterval(function(){
     });
 }, 1000*15);
 
-// manages page resizing
-window.addEventListener('resize', function(event){
+function managePageResizing(){
     if ($(window).width() < 800){
         // SMALL
         // title fades out
@@ -29,7 +28,7 @@ window.addEventListener('resize', function(event){
             $('#title').css('display', 'None');
         });
         // navigator buttons less spaced
-        $('.navigators').find('a').css('padding', '5px 1vw 5px 1vw');
+        $('.navigators').find('a').css('padding', '5px 10px 5px 10px');
         // navigator right space removed
         $('.navigators').css('right', '5px');
     }else{
@@ -39,8 +38,34 @@ window.addEventListener('resize', function(event){
             $('#title').css('display', 'Block');
         });
         // navigator buttons are wider spaced
-        $('.navigators').find('a').css('padding', '5px 2vw 5px 2vw');
+        $('.navigators').find('a').css('padding', '5px 30px 5px 30px');
         // navigator section gets right space
         $('.navigators').css('right', '50px');
     }
+}
+
+// manages page resizing
+window.addEventListener('resize', function(event){
+    managePageResizing();
 });
+
+
+// manages cycle throughs
+
+next = false;
+window.setInterval(function(){
+    $(".cycle_through").each(function(){
+        $(this).fadeOut( "slow", function(a) {
+            elem = $(this).find(".cycle")
+            elem.removeClass("cycle");
+            index = elem.index();
+            if (index < $(this).children().length -1){
+                $(this).children()[index +1].classList.add("cycle");
+            }else{
+                $(this).children()[0].classList.add("cycle");
+            }
+        });
+        $(this).fadeIn( "slow", function(a) {});
+    })
+
+}, 1000*10);
